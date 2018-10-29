524b34298a73cddf88e4761748c6a2c695059b21
asleboon <asle.berge@gmail.com>
2018-10-29 14:25:39 +0100

    Trim strings in incoming event string

--- chzap/chzap.go
+++ chzap/chzap.go
@@ -39,29 +39,33 @@ func NewSTBEvent(event string) (*ChZap, *StatusChange, error) {
 	// ChZap format: {"2010/12/22, 20:22:32, 10.213.223.232, NRK2, NRK1", "20:22:32"}
 	// StatusChange format: "2013/07/20, 21:57:42, 203.124.29.72, Volume: 50"}
 
-	eventStr := strings.Split(event, ",")
-	switch len(eventStr) {
+	eventeSlice := strings.Split(event, ",")
+	for _, str := range eventeSlice {
+		strings.Trim(str, " ")
+	}
+
+	switch len(eventeSlice) {
 	case 5: // ChZap
-		eventTime, err := time.Parse(timeFormat, eventStr[0]+","+eventStr[1])
+		eventTime, err := time.Parse(timeFormat, eventeSlice[0]+","+eventeSlice[1])
 		if err != nil {
 			err = fmt.Errorf("NewSTBEvent: failed to parse timestamp")
 			return nil, nil, err
 		}
-		chZap := ChZap{Time: eventTime, IP: eventStr[2], ToChan: eventStr[3], FromChan: eventStr[4]}
+		chZap := ChZap{Time: eventTime, IP: eventeSlice[2], ToChan: eventeSlice[3], FromChan: eventeSlice[4]}
 		return &chZap, nil, err
 	case 4: // Statuschange
-		eventTime, err := time.Parse(timeFormat, eventStr[0]+","+eventStr[1])
+		eventTime, err := time.Parse(timeFormat, eventeSlice[0]+","+eventeSlice[1])
 		if err != nil {
 			err = fmt.Errorf("NewSTBEvent: failed to parse timestamp")
 			return nil, nil, err
 		}
-		staCha := StatusChange{Time: eventTime, Volume: eventStr[1], MuteStatus: eventStr[2], HDMIStatus: eventStr[3]}
+		staCha := StatusChange{Time: eventTime, Volume: eventeSlice[1], MuteStatus: eventeSlice[2], HDMIStatus: eventeSlice[3]}
 		return nil, &staCha, nil
 	case 3: // Error
-		err := fmt.Errorf("NewSTBEvent: event with too few fields: %s,%s,%s", eventStr[0], eventStr[1], eventStr[2])
+		err := fmt.Errorf("NewSTBEvent: event with too few fields: %s,%s,%s", eventeSlice[0], eventeSlice[1], eventeSlice[2])
 		return nil, nil, err
 	case 2: // Error
-		err := fmt.Errorf("NewSTBEvent: too short event string: %s,%s", eventStr[0], eventStr[1])
+		err := fmt.Errorf("NewSTBEvent: too short event string: %s,%s", eventeSlice[0], eventeSlice[1])
 		return nil, nil, err
 	default:
 		// What is default case?
@@ -73,11 +77,11 @@ func NewSTBEvent(event string) (*ChZap, *StatusChange, error) {
 }
 
 func (zap ChZap) String() string {
-	return fmt.Sprintf("%s%s%s%s", zap.Time, zap.IP, zap.ToChan, zap.FromChan)
+	return fmt.Sprintf("%s %s %s %s", zap.Time, zap.IP, zap.ToChan, zap.FromChan)
 }
 
 func (schg StatusChange) String() string {
-	return fmt.Sprintf("%s%s%s%s", schg.Time, schg.Volume, schg.MuteStatus, schg.HDMIStatus)
+	return fmt.Sprintf("%s %s %s %s", schg.Time, schg.Volume, schg.MuteStatus, schg.HDMIStatus)
 }
 
 // Duration returns between two zap events: The receiving (this) zap event and the provided event.
