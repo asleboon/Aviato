e4a8cb1d7cb0599173964f6f813123fd2f57018d
Mats Tyldum <ma.tyldum@gmail.com>
2018-11-03 16:51:41 +0100

    Continued with grpc server and client implementation

--- grpc/client/subscribeClient.go
+++ grpc/client/subscribeClient.go
@@ -1,29 +1,79 @@
 package main
 
 import (
-	"bufio"
+	"context"
 	"fmt"
 	"log"
-	"os"
 
 	pb "github.com/uis-dat320-fall18/Aviato/proto"
 	"google.golang.org/grpc"
 )
 
-const addr = "localhost:xxxxx"
+const addr = "localhost:12111"
 
+/* Remove?
 func subscribe(s pb.SubscriptionClient, ch string, rate int) (string, error) {
 	//
 	return "test", nil
 }
+*/
 
-func main() {
-	// copied from lab3
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf("reader: %q", reader)
-	conn, err := grpc.Dial(addr, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("could not subscribe %v", err)
+// Look at SubscriptionClient API in subscribe.pb.go
+type SubscriptionClient interface {
+	Subscribe(ctx context.Context, opts ...grpc.CallOption) (Subscription_SubscribeClient, error)
+}
+
+type subscriptionClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewSubscriptionClient(cc *grpc.ClientConn) pb.SubscriptionClient {
+	//return &subscriptionClient{cc}
+	return nil // Remove
+}
+
+func (c *subscriptionClient) Subscribe(ctx context.Context, opts ...grpc.CallOption) (Subscription_SubscribeClient, error) {
+	/*
+		stream, err := c.cc.NewStream(ctx, &_Subscription_serviceDesc.Streams[0], "/proto.Subscription/Subscribe", opts...)
+		if err != nil {
+			return nil, err
+		}
+		x := &subscriptionSubscribeClient{stream}
+		return x, nil
+	*/
+	return nil, nil // Remove
+}
+
+type Subscription_SubscribeClient interface {
+	Send(*pb.SubscribeMessage) error
+	Recv() (*pb.NotificationMessage, error)
+	grpc.ClientStream
+}
+
+type subscriptionSubscribeClient struct {
+	grpc.ClientStream
+}
+
+func (x *subscriptionSubscribeClient) Send(m *pb.SubscribeMessage) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *subscriptionSubscribeClient) Recv() (*pb.NotificationMessage, error) {
+	m := new(pb.NotificationMessage)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
 	}
-	fmt.Printf("conn: %q", conn)
+	return m, nil
+}
+
+// Main func inspired by lab3
+func main() {
+	// reader := bufio.NewReader(os.Stdin)               // Why do we need a reader?
+	// fmt.Printf("reader: %q", reader)                  // And why is this needed?
+	conn, err := grpc.Dial(addr, grpc.WithInsecure()) // WithInsecure: Disable transport security connection
+	if err != nil {
+		log.Fatalf("Error with creating connection to gRPC server: %v", err)
+	}
+	fmt.Printf("Connection to gRPC server created: %v", conn)
+	defer conn.Close() // Closing connection when the surrounding function return
 }
--- grpc/server/subscribeServer.go
+++ grpc/server/subscribeServer.go
@@ -1,22 +1,109 @@
 package main
 
 import (
-	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
 	"sync"
 
-	//"google.golang.org/grpc"
 	pb "github.com/uis-dat320-fall18/Aviato/proto"
+	"google.golang.org/grpc"
 )
 
+// SubscribeServer exported? Or not exported?
 type SubscribeServer struct {
 	kv    map[string]int // store channels and number of viewers?
 	mutex sync.Mutex
 }
 
+var (
+	help = flag.Bool(
+		"help",
+		false,
+		"Show usage help",
+	)
+	endpoint = flag.String(
+		"endpoint",
+		"localhost:12111",
+		"Endpoint on which server runs or to which client connects",
+	)
+)
+
+func Usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS]\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\nOptions:\n")
+	flag.PrintDefaults()
+}
+
+/* Remove?
 // subscribe.proto: rpc Subscribe(stream SubscribeMessage) returns (stream NotificationMessage)
 func (ss *SubscribeServer) Subscribe(ctx context.Context, req *pb.SubscribeMessage) (*pb.NotificationMessage, error) {
 	return nil, nil
 }
+*/
 
+// Look at SubscriptionServer API in subscribe.pb.go
+type SubscriptionServer interface {
+	Subscribe(Subscription_SubscribeServer) error
+}
+
+func RegisterSubscriptionServer(s *grpc.Server, srv SubscriptionServer) {
+	// s.RegisterService(&_Subscription_serviceDesc, srv)
+}
+
+func _Subscription_Subscribe_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(SubscriptionServer).Subscribe(&subscriptionSubscribeServer{stream})
+}
+
+type Subscription_SubscribeServer interface {
+	// Send(*NotificationMessage) error
+	// Recv() (*SubscribeMessage, error)
+	grpc.ServerStream
+}
+
+type subscriptionSubscribeServer struct {
+	grpc.ServerStream
+}
+
+func (x *subscriptionSubscribeServer) Send(m *pb.NotificationMessage) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *subscriptionSubscribeServer) Recv() (*pb.SubscribeMessage, error) {
+	m := new(pb.SubscribeMessage)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+// Main func inspired by lab3
 func main() {
+	flag.Usage = Usage
+	flag.Parse()
+	if *help {
+		flag.Usage()
+		return
+	}
+
+	listener, err := net.Listen("tcp", *endpoint)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	} else {
+		fmt.Printf("Listener started on %v\n", *endpoint)
+	}
+
+	server := new(SubscribeServer)
+	server.kv = make(map[string]int)
+	grpcServer := grpc.NewServer()
+
+	// Must create proto first. cmd: protoc --go_out=plugins=grpc:. subscribe.proto
+	//pb.RegisterKeyValueServiceServer(grpcServer, server)
+
+	fmt.Printf("Preparing to serve incoming requests.\n")
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
 }
