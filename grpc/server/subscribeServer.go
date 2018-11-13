db1a4d3cb68d6b85f1bd86d0e0ac4f93b22862bf
Mats Tyldum <ma.tyldum@gmail.com>
2018-11-13 12:52:06 +0100

    Update grpc/server/subscribeServer.go

--- grpc/server/subscribeServer.go
+++ grpc/server/subscribeServer.go
@@ -190,8 +190,6 @@ func (s *SubscribeServer) Subscribe(stream pb.Subscription_SubscribeServer) erro
 				resString = s.top10Duration()
 			} else if in.StatisticsType == "mute" {
 				resString = s.top10Mute()
-			} else if in.StatisticsType == "SMA" {
-				resString = s.SMA(in.SMAChannel, in.SMALength) // Have to compile proto file again
 			}
 
 			err := stream.Send(&pb.NotificationMessage{Top10: resString})
