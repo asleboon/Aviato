5060145cb38ca46bfc94c11b9bb05373937df900
Mats Tyldum <ma.tyldum@gmail.com>
2018-11-16 14:23:05 +0100

    Update zapserver.go

--- zapserver/zapserver.go
+++ zapserver/zapserver.go
@@ -165,7 +165,7 @@ func calculateTop10Muted() []*zlog.ChannelViewers {
 }
 
 func drawChart(channelName string) {
-	fmt.Printf("Drawing chart for %v", channelName)
+	fmt.Printf("Drawing chart for %v\n", channelName)
 	views, times := []float64{}, []time.Time{}
 	tickChan := time.NewTicker(time.Second * 60) // TODO: Change to time.Hour * 24
 	defer tickChan.Stop()
