32c2c3aa11083804647245b87353789e6bf252a0
Mats Tyldum <ma.tyldum@gmail.com>
2018-11-17 16:29:36 +0100

    Update zapserver.go

--- zapserver/zapserver.go
+++ zapserver/zapserver.go
@@ -170,7 +170,7 @@ func calculateTop10Muted() []*zlog.ChannelViewers {
 // drawChart creates three charts for viewers from two channels. One for each and one combined
 func drawChart(channelOne string, channelTwo string) {
 	viewsOne, timesOne, viewsTwo, timesTwo := []float64{}, []time.Time{}, []float64{}, []time.Time{}
-	tickChan := time.NewTicker(time.Minute * 1)
+	tickChan := time.NewTicker(time.Minute * 30)
 	defer tickChan.Stop()
 	for range tickChan.C { // Runs code inside loop every 30min
 		log.Printf("Drawing chart for '%v' and '%v'...\n", channelOne, channelTwo)
